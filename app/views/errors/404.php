fd277233550fb09a61f5a19a9b87bd2373c6fa9b
Nguyen Duc Bao <you@example.com>
2026-06-02 09:33:07 +0700

    add code

--- app/views/errors/404.php
+++ app/views/errors/404.php
@@ -1,4 +1,6 @@
-<section class="content-block">
-    <h1>404</h1>
-    <p>Không tìm thấy trang.</p>
-</section>
+<div class="container">
+    <section class="content-block" style="height:100vh;">
+        <h1>404</h1>
+        <p>Không tìm thấy trang.</p>
+    </section>
+</div>
