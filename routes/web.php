874f63f15e085d8bd6126d49717a1bfa656840b2
doubleD1302 <channelpha2005@gmail.com>
2026-06-09 02:01:02 +0700

    Fix merge conflict and cleanup routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -1,42 +1,28 @@
 <?php
 
 return [
-	'login' => [
-		'title' => 'Đăng nhập',
-		'view' => 'auth/login.php',
-
-	],
-	'register' => [
-		'title' => 'Đăng ký',
-		'view' => 'auth/register.php',
-
-	],
-	'home' => [
-		'title' => 'Trang chủ',
-		'view' => 'home/index.php',
-
-	],
-	'product' => [
-		'title' => 'Sản phẩm',
-		'view' => 'product/index.php',
-	],
-<<<<<<< HEAD
-=======
-	'login' => [
-		'title' => 'Đăng Nhập',
-		'view' => 'auth/login.php',
-	],
-	'register' => [
-		'title' => 'Đăng Ký',
-		'view' => 'auth/register.php',
-	],
-	'about' => [
-		'title' => 'Giới thiệu',
-		'view' => 'about/about.php',
-	],
->>>>>>> ducdat
-	'404' => [
-		'title' => 'Không tìm thấy trang',
-		'view' => 'errors/404.php',
-	],
+    'home' => [
+        'title' => 'Trang chủ',
+        'view' => 'home/index.php',
+    ],
+    'product' => [
+        'title' => 'Sản phẩm',
+        'view' => 'product/index.php',
+    ],
+    'about' => [
+        'title' => 'Giới thiệu',
+        'view' => 'about/about.php',
+    ],
+    'login' => [
+        'title' => 'Đăng Nhập',
+        'view' => 'auth/login.php',
+    ],
+    'register' => [
+        'title' => 'Đăng Ký',
+        'view' => 'auth/register.php',
+    ],
+    '404' => [
+        'title' => 'Không tìm thấy trang',
+        'view' => 'errors/404.php',
+    ],
 ];
